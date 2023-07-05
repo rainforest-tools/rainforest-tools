c3b39c4a3c7305cb02f2730a634551d21bf6837c
Rainforest <rainforestnick@gmail.com>
2023-07-05 10:53:00 +0000

    feat(three.js journey): add flag texture material

--- src/shaders/28/fragment.glsl
+++ src/shaders/28/fragment.glsl
@@ -1,7 +1,13 @@
 precision mediump float;
 
-varying float vRandom;
+uniform vec3 uColor;
+uniform sampler2D uTexture;
+
+varying vec2 vUv;
+varying float vElevation;
 
 void main() {
-    gl_FragColor = vec4(1.0, 0.0, 0.0, vRandom);
+    vec4 textureColor = texture2D(uTexture, vUv);
+    textureColor.rgb *= vElevation * 2.0 + 0.5;
+    gl_FragColor = textureColor;
 }
