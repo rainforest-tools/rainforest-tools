3f2b23714fdbf23822b98441d78f1a3aa676be0e
Rainforest <rainforestnick@gmail.com>
2023-07-05 08:42:34 +0000

    feat(three.js journey): aRandom, vRandom and import glsl

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,12 @@ import react from "@astrojs/react";
 
 import tailwind from "@astrojs/tailwind";
 
+import glsl from 'vite-plugin-glsl'
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind(), glsl()],
+  vite: {
+    plugins: [glsl()]
+  }
 });
